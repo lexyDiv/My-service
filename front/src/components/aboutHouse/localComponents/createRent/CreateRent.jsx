f289297d1852fe48758983e94f0f824ff0f95dd2
lexyDiv <aledab@mail.ru>
2024-07-25 11:54:20 +0300

    before re buttons

--- front/src/components/aboutHouse/localComponents/createRent/CreateRent.jsx
+++ front/src/components/aboutHouse/localComponents/createRent/CreateRent.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Calendar2 from "../../../Calendars/Calendar2";
 import './CreateRent.css'
+import RentCalendar from "./localComponents/rentCalendar/RentCalendar";
 
 const CreateRent = function({ house, user }) {
     return (
         <div id="create-rent">
-          <Calendar2/>
+          {/* <Calendar2/> */}
+          <RentCalendar/>
         </div>
     )
 }
