dc825ac8bb3becc39766c857f65cd4cb5963f1a3
lexyDiv <aledab@mail.ru>
2024-07-30 07:57:52 +0300

    mid ok

--- front/src/components/aboutHouse/localComponents/createRent/localComponents/rentCalendar/functions/onDraw.js
+++ front/src/components/aboutHouse/localComponents/createRent/localComponents/rentCalendar/functions/onDraw.js
@@ -4,6 +4,56 @@ export function onDraw(el, reservesDB, focusRent) {
   if (cal) {
     for (let i = 0; i < cal.childNodes.length; i++) {
       const div = cal.childNodes[i];
+      if (!div.update) {
+        div.update = "ok";
+        const leftDiv = document.createElement("div");
+        leftDiv.className = "left-div";
+        const rightDiv = document.createElement("div");
+        rightDiv.className = "right-div";
+        const rightDivNext = document.createElement("div");
+        rightDivNext.className = "right-div-next";
+        //console.log(leftDiv)
+        div.appendChild(leftDiv);
+        div.appendChild(rightDiv);
+        div.appendChild(rightDivNext);
+      }
+    }
+
+    for (let i = 0; i < cal.childNodes.length; i++) {
+      const div = cal.childNodes[i];
+      div.style.color = "";
+      let cNLength = div.childNodes.length;
+      let leftDiv = null;
+      let rightDiv = null;
+      let rightDivNext = null;
+
+      for (let h = 0; h < cNLength; h++) {
+        const childDiv = div.childNodes[h];
+        if (childDiv.className === "left-div") {
+          leftDiv = childDiv;
+        } else if (childDiv.className === "right-div") {
+          rightDiv = childDiv;
+        } else if (childDiv.className === "right-div-next") {
+          rightDivNext = childDiv;
+        }
+      }
+
+      rightDiv.style.backgroundColor = "";
+      leftDiv.style.backgroundColor = "";
+      rightDivNext.style.backgroundColor = "";
+      leftDiv.style.borderTopLeftRadius = "0px";
+      leftDiv.style.borderBottomLeftRadius = "0px";
+      leftDiv.style.borderTopRightRadius = "0px";
+      leftDiv.style.borderBottomRightRadius = "0px";
+      rightDiv.style.borderTopLeftRadius = "0px";
+      rightDiv.style.borderBottomLeftRadius = "0px";
+      rightDiv.style.borderTopRightRadius = "0px";
+      rightDiv.style.borderBottomRightRadius = "0px";
+      rightDivNext.style.borderTopLeftRadius = "0px";
+      rightDivNext.style.borderBottomLeftRadius = "0px";
+      rightDivNext.style.borderTopRightRadius = "0px";
+      rightDivNext.style.borderBottomRightRadius = "0px";
+
       let color = "";
       for (let k = 0; k < reservesDB.length; k++) {
         const reserv = reservesDB[k];
@@ -11,39 +61,32 @@ export function onDraw(el, reservesDB, focusRent) {
         for (let j = 0; j < days.length; j++) {
           const reservedDay = days[j];
           if (reservedDay === div.ariaLabel) {
-            if (
-              focusRent &&
-              JSON.parse(focusRent.days).find((day) => day === reservedDay)
-            ) {
-              div.style.borderStyle = "solid";
-            } else {
-              div.style.borderStyle = "";
-            }
+            div.style.color = "black";
+            color = reserv.type === "go" ? "red" : "yellow";
+
             if (!j) {
-              div.style.borderTopLeftRadius = "20px";
-              div.style.borderBottomLeftRadius = "20px";
-            }
-            if (j === days.length - 1) {
-              div.style.borderTopRightRadius = "20px";
-              div.style.borderBottomRightRadius = "20px";
+              rightDiv.style.backgroundColor = color;
+              rightDiv.style.borderTopLeftRadius = "20px";
+              rightDiv.style.borderBottomLeftRadius = "20px";
             }
 
-            color = reserv.type === "go" ? "red" : "yellow";
+            if (j === days.length - 1) {
+              rightDivNext.style.backgroundColor = color;
+              rightDivNext.style.borderTopRightRadius = "20px";
+              rightDivNext.style.borderBottomRightRadius = "20px";
+            }
+
+            if (j && j <= days.length - 1) {
+              leftDiv.style.backgroundColor = color;
+              rightDiv.style.backgroundColor = color;
+            }
             break;
-          } else {
-            div.style.borderStyle = "none";
-            div.style.borderTopLeftRadius = "0px";
-            div.style.borderBottomLeftRadius = "0px";
-            div.style.borderTopRightRadius = "0px";
-            div.style.borderBottomRightRadius = "0px";
           }
         }
         if (color) {
           break;
         }
       }
-      div.style.backgroundColor = color;
-      // div.style.borderRadius = "10px";
     }
   }
 }
