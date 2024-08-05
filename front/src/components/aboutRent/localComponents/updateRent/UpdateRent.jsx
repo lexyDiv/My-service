37455c51ed7d3a8f400837cce3445baef0a89cec
lexyDiv <aledab@mail.ru>
2024-08-05 19:49:09 +0300

    before re Update rent

--- front/src/components/aboutRent/localComponents/updateRent/UpdateRent.jsx
+++ front/src/components/aboutRent/localComponents/updateRent/UpdateRent.jsx
@@ -12,14 +12,26 @@ const UpdateRent = function ({ rent }) {
   const { locationId, houseId, rentId } = useParams();
   const type = rent.type === "hold" ? "забронировано" : "сдано";
   const [status, setStatus] = useState(type);
-  const [client, setClient] = useState(rent.Client);
+  //const [client, setClient] = useState(rent.Client);
+  const [clientStatus, setClientStatus] = useState("");
 
   const typeDataArr = ["забронировано", "сдано"];
-  const clientDataArr = ["не определён", "создать", "найти"];
+  const clientDataArr = ["создать", "найти"];
+  rent.Client && clientDataArr.push("не определён");
 
   const typeCB = (type) => {
     setStatus(type);
   };
+
+  const clientCB = (type) => {
+    setClientStatus(type);
+   console.log(type)
+  }
+
+  const statusQO = () => {
+    setClientStatus("");
+    setStatus(type);
+  }
 
   return (
     <div id="update-rent">
@@ -37,19 +49,28 @@ const UpdateRent = function ({ rent }) {
         />
       </div>
       <div id="update-rent-client">
-        <p className="update-rent-item" >Клиент:</p>
-        {client ? (
-          <div>client</div>
-        ) : (
-          <p className="update-rent-item" style={{ color: "red" }}>не определён</p>
+        <p className="update-rent-item">Клиент:</p>
+        {(!rent.Client || clientStatus === "не определён") && (
+          <p className="update-rent-item" style={{ color: "red" }}>
+            не определён
+          </p>
         )}
         <DialogWindow
-          dataArr={typeDataArr.filter((el) => el !== status)}
-          cb={typeCB}
+          dataArr={clientDataArr}
+          cb={clientCB}
         />
       </div>
-      {typeKeys[status] !== rent.type && (
-        <button type="button">сохранить изменения</button>
+      {rent.Client && clientStatus !== "не определён" && <div>client here</div>}
+      {(typeKeys[status] !== rent.type
+        ||
+        (rent.Client && clientStatus === "не определён")
+      ) && (
+        <div>
+          <button type="button">сохранить изменения</button>
+          <button 
+          onClick={statusQO}
+          type="button">отменить изменения</button>
+        </div>
       )}
     </div>
   );
