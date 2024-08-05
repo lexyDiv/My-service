f0397e67609ababcd6aa24648c539418cf7344a0
lexyDiv <aledab@mail.ru>
2024-08-05 07:31:02 +0300

    navBtn locationList ok

--- front/src/components/locationList/LocationList.jsx
+++ front/src/components/locationList/LocationList.jsx
@@ -1,33 +1,37 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 
-
 import "./LocationList.css";
 import ScrollContainer from "../scrollContainer/ScrollContainer";
 import NavBtn from "../navBtn/NavBtn";
 import { useSetContent } from "./functions/useSetContent";
 import { setLocalPageProg } from "./functions/setLocalPageProg";
 
-const pages = ['наши базы', 'создать базу'];
+const pages = ["наши базы", "создать базу"];
 
 const LocationList = function () {
-
   const [localPage, setLocalPage] = useState(pages[0]);
-  const dispatch = useDispatch();
+  //const dispatch = useDispatch();
 
-  useEffect(() => {
-   // dispatch({ type: "FIRST", payload: [{ name: "БАЗЫ", path: "/", id: 0 }] });
-  }, [dispatch]);
+  // useEffect(() => {
+  //   // dispatch({ type: "FIRST", payload: [{ name: "БАЗЫ", path: "/", id: 0 }] });
+  // }, [dispatch]);
 
   const constCallBack = useSetContent(localPage);
 
- const cb = () => {
-  setLocalPageProg(setLocalPage, pages);
- }
+  const cb = (e) => {
+    //setLocalPageProg(setLocalPage, pages);
+    setLocalPage(e.target.innerText);
+    
+  };
 
   return (
     <div className="locations-box">
-      <NavBtn text={localPage} cb={cb}/>
+      <NavBtn
+        text={localPage}
+        cb={cb}
+        pages={pages.filter((el) => el !== localPage)}
+      />
       <ScrollContainer contCallBack={constCallBack} />
     </div>
   );
