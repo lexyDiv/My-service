2852b39f486f325372240d0cb544634e859188fd
lexyDiv <aledab@mail.ru>
2024-08-06 14:48:24 +0300

    before update-rent

--- front/src/components/aboutRent/localComponents/updateRent/UpdateRent.jsx
+++ front/src/components/aboutRent/localComponents/updateRent/UpdateRent.jsx
@@ -1,9 +1,12 @@
-import React, { useState } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useRef, useState } from "react";
 import "./UpdateRent.css";
 import { useParams } from "react-router-dom";
 import DialogWindow from "./localComponents/DialogWindow";
 import ClientItem from "../../../clientItem/ClientItem";
-
+import { useDispatch } from "react-redux";
+import { getOneClient } from "../../../../functions/getOneClient";
+import Find from "./localComponents/find/Find";
 
 const typeKeys = {
   забронировано: "hold",
@@ -13,12 +16,32 @@ const typeKeys = {
 const UpdateRent = function ({ rent }) {
   const { locationId, houseId, rentId } = useParams();
   const type = rent.type === "hold" ? "забронировано" : "сдано";
+  const clientRef = useRef(null);
   const [status, setStatus] = useState(type);
-  const [client, setClient] = useState(rent.Client);
+  const [client, setClient] = useState(null);
   const [clientStatus, setClientStatus] = useState("");
+  const dispatch = useDispatch();
+
+  async function getClient() {
+    dispatch({ type: "SET_LOADING", payload: true });
+    const clientData = await getOneClient(rent.client_id);
+    if (clientData) {
+      setClient(clientData);
+      clientRef.current = clientData;
+      rent.Client = clientData;
+    }
+    dispatch({ type: "SET_LOADING", payload: false });
+  }
+
+  useEffect(() => {
+    getClient();
+  }, []);
 
   const typeDataArr = ["забронировано", "сдано"];
-  const clientDataArr = ["создать", "найти"];
+  let clientDataArr = ["найти"];
+  if (clientStatus === "найти" || clientRef.current !== client) {
+    clientDataArr.push("по умолчанию");
+  }
   client && clientDataArr.push("не определён");
 
   const typeCB = (type) => {
@@ -26,19 +49,26 @@ const UpdateRent = function ({ rent }) {
   };
 
   const clientCB = (type) => {
-    if(type === "не определён") {
+    if (type === "не определён") {
       setClient(null);
+      setClientStatus("");
+    } else if (type === "по умолчанию") {
+      setClient(clientRef.current);
+      setClientStatus("");
+    } else if (type === "найти") {
+      setClientStatus(type);
+      setClient(clientRef.current);
     } else {
       setClientStatus(type);
     }
-   console.log(type)
-  }
+    console.log(type);
+  };
 
   const statusQO = () => {
     setClientStatus("");
-    setClient(rent.Client);
+    setClient(clientRef.current);
     setStatus(type);
-  }
+  };
 
   return (
     <div id="update-rent">
@@ -57,26 +87,24 @@ const UpdateRent = function ({ rent }) {
       </div>
       <div id="update-rent-client">
         <p className="update-rent-item">Клиент:</p>
-        {(!client) && (
+        {!client && (
           <p className="update-rent-item" style={{ color: "red" }}>
             не определён
           </p>
         )}
         <DialogWindow
-          dataArr={clientDataArr}
+          dataArr={clientDataArr.filter((el) => el !== clientStatus)}
           cb={clientCB}
         />
       </div>
-      {client && <ClientItem client={client}/>}
-      {(typeKeys[status] !== rent.type
-        ||
-        (rent.Client !== client)
-      ) && (
+      {client && clientStatus !== "найти" && <ClientItem client={client} />}
+      {clientStatus === "найти" && <Find />}
+      {(typeKeys[status] !== rent.type || clientRef.current !== client) && (
         <div>
           <button type="button">сохранить изменения</button>
-          <button 
-          onClick={statusQO}
-          type="button">отменить изменения</button>
+          <button onClick={statusQO} type="button">
+            отменить изменения
+          </button>
         </div>
       )}
     </div>
