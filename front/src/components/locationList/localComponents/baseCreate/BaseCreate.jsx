4e2b4f2da9bcd78bce5a5419df0adcaaafdbc247
lexyDiv <aledab@mail.ru>
2024-08-20 11:35:02 +0300

    before base photo add

--- front/src/components/locationList/localComponents/baseCreate/BaseCreate.jsx
+++ front/src/components/locationList/localComponents/baseCreate/BaseCreate.jsx
@@ -1,62 +1,108 @@
 import React, { useState } from "react";
-import './BaseCreate.css';
+import "./BaseCreate.css";
 import { createTheme, TextField, ThemeProvider } from "@mui/material";
+import {
+  nameValidator,
+  nameValidatorCenter,
+  nameValidatorEnd,
+  nameValidatorStart,
+} from "../../../../functions/nameValidator";
 
+const BaseCreate = function () {
+  const theme = createTheme({
+    palette: {
+      background: {
+        paper: "#212121",
+      },
+      text: {
+        primary: "rgb(255,255,255)",
+        secondary: "rgb(255,255,255)",
+      },
+      action: {
+        active: "#001E3C",
+      },
+    },
+  });
 
-const BaseCreate = function() {
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+  const [description, setDescription] = useState("");
 
-    const theme = createTheme({
-        palette: {
-          background: {
-            paper: "#212121",
-          },
-          text: {
-            primary: "rgb(255,255,255)",
-            secondary: "rgb(255,255,255)",
-          },
-          action: {
-            active: "#001E3C",
-          },
-        },
-      });
+  const rand = Math.floor(Math.random() * 10000);
 
-      const [name, setName] = useState("");
-
-
-      const rand  = Math.floor(Math.random() * 10000);
-
-    return (
-        <ThemeProvider theme={theme}>
-        <div id="base-create">
-                   <div className="create-client-basic-item">
-         <TextField
-         value={name}
-         // onChange={(e) => setName(e.target.value)}
-          autoComplete="false"
-        //   onFocus={() => {
-        //     phone.length === 2 && setPhone("");
-        //     tele.length === 1 && setTele("");
-        //   }}
-          sx={{
-            "& fieldset.MuiOutlinedInput-notchedOutline": {
-              borderColor: "rgb(255,255,255)",
-            },
-            width: "90%",
-          }}
-          id={"outlined-basic-1" + rand}
-          label="Название новой базы"
-          variant="outlined"
-        />
-        <div
-              style={{
-                backgroundColor: 'green',
-              }}
-              className="create-client-basic-item-ok"
-            />
-         </div>
+  return (
+    <ThemeProvider theme={theme}>
+      <div id="base-create">
+        <div className="create-client-basic-item">
+          <TextField
+            value={name}
+            onChange={(e) => setName(nameValidatorStart(e.target.value))}
+            autoComplete="false"
+            sx={{
+              "& fieldset.MuiOutlinedInput-notchedOutline": {
+                borderColor: "rgb(255,255,255)",
+              },
+              width: "90%",
+            }}
+            id={"outlined-basic-1" + rand}
+            label="Название новой базы"
+            variant="outlined"
+          />
+          <div
+            style={{
+              backgroundColor: "green",
+            }}
+            className="create-client-basic-item-ok"
+          />
         </div>
-        </ThemeProvider>
-    )
-}
+        <div className="create-client-basic-item">
+          <TextField
+            value={address}
+            onChange={(e) => setAddress(nameValidatorStart(e.target.value))}
+            autoComplete="false"
+            sx={{
+              "& fieldset.MuiOutlinedInput-notchedOutline": {
+                borderColor: "rgb(255,255,255)",
+              },
+              width: "90%",
+            }}
+            id={"outlined-basic-1" + rand}
+            label="Адрес новой базы"
+            variant="outlined"
+          />
+          <div
+            style={{
+              backgroundColor: "green",
+            }}
+            className="create-client-basic-item-ok"
+          />
+        </div>
+        <div className="create-client-basic-item">
+          <TextField
+            value={description}
+            onChange={(e) => setDescription(nameValidatorStart(e.target.value))}
+            autoComplete="false"
+            multiline
+            sx={{
+              "& fieldset.MuiOutlinedInput-notchedOutline": {
+                borderColor: "rgb(255,255,255)",
+              },
+              width: "90%",
+            }}
+            id={"outlined-basic-1" + rand}
+            label="Описание новой базы"
+            variant="outlined"
+          />
+          <div
+            style={{
+              backgroundColor: "green",
+            }}
+            className="create-client-basic-item-ok"
+          />
+        </div>
+      </div>
+    </ThemeProvider>
+  );
+};
 
 export default BaseCreate;
