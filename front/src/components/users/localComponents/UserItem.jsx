a527aed9f322dbfe1ade56e034b7ba0850f8ae74
lexyDiv <aledab@mail.ru>
2024-08-29 11:30:41 +0300

    before user cabinet

--- front/src/components/users/localComponents/UserItem.jsx
+++ front/src/components/users/localComponents/UserItem.jsx
@@ -5,12 +5,18 @@ import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 import ButtonWithQuestion from "../../buttonWithQuestion/ButtonWithQuestion";
 import { useSetUserFetch } from "./functions/useSetUserFetch";
 import ReplyIcon from "@mui/icons-material/Reply";
+import ChangeCircleIcon from "@mui/icons-material/ChangeCircle";
 
 const UserItem = function ({ userPers, setUsers }) {
   const { user } = useSelector((store) => store.user);
 
   const setUserOutFetch = useSetUserFetch({ level: 0, setUsers, userPers });
-  const setUserInFetch = useSetUserFetch({ level: 2, setUsers, userPers });
+  const setUserInFetch = useSetUserFetch({ level: 1, setUsers, userPers });
+  const setUserLevelFetch = useSetUserFetch({
+    level: userPers.level === 1 ? 2 : 1,
+    setUsers,
+    userPers,
+  });
 
   return (
     <div className="user-item">
@@ -39,7 +45,37 @@ const UserItem = function ({ userPers, setUsers }) {
           </p>
         )}
         {!userPers.admin && userPers.level ? (
-          <div style={{ color: "rgb(6, 252, 6)" }}>
+          <div className="user-level">
+            <p
+              style={{
+                color: "rgb(173, 209, 240)",
+                margin: "12px",
+              }}
+            >
+              Доступ:
+            </p>
+            <p style={{ margin: "12px" }}>{userPers.level}</p>
+            {user.admin && (
+              <ButtonWithQuestion
+                menuPunkt={[
+                  { page: "да", color: "red", cb: setUserLevelFetch },
+                  {
+                    page: "нет",
+                    color: "green",
+                    cb: (hc) => {
+                      hc();
+                    },
+                  },
+                ]}
+                buttonContent={() => <ChangeCircleIcon />}
+              />
+            )}
+          </div>
+        ) : (
+          false
+        )}
+        {!userPers.admin && userPers.level ? (
+          <div style={{ color: "rgb(6, 252, 6)", marginTop: "10px" }}>
             работает
             {user && user.admin && (
               <ButtonWithQuestion
