bf8377248f2365fc34cdee4718e06e6d76b795bd
lexyDiv <aledab@mail.ru>
2024-09-17 08:55:36 +0300

    before new update

--- front/src/components/aboutHouse/localComponents/createRent/localComponents/rentCalendar/functions/isValideSecondClick.js
+++ front/src/components/aboutHouse/localComponents/createRent/localComponents/rentCalendar/functions/isValideSecondClick.js
@@ -1,19 +1,21 @@
 import { getApparatDate } from "../../../../../../aboutRent/localComponents/updateRent/localComponents/updateCalendar/functions/onDrawUpdateCalendar";
 
-export const isValideSecondClick = function(newInterval, clickTime, rents) {
-    const nStartTime = newInterval.startTime;
-    const cTime = getApparatDate(clickTime);
-    for(let i = 0; i < rents.length; i++) {
-        const rent = rents[i];
-        const rStartTime = Number(rent.startTime);
-        const rEndTime = Number(rent.endTime);
-        if (
-            (cTime < nStartTime)
-            || (rStartTime > nStartTime && rStartTime <= cTime)
-            || (rEndTime > nStartTime && rEndTime <= cTime)
-        ) {
-            return false;
-        }
+export const isValideSecondClick = function (newInterval, clickTime, rents) {
+  const nStartTime = newInterval.startTime;
+  const cTime = getApparatDate(clickTime);
+  if (cTime < nStartTime) {
+    return false;
+  }
+  for (let i = 0; i < rents.length; i++) {
+    const rent = rents[i];
+    const rStartTime = Number(rent.startTime);
+    const rEndTime = Number(rent.endTime);
+    if (
+      (rStartTime > nStartTime && rStartTime <= cTime) ||
+      (rEndTime > nStartTime && rEndTime <= cTime)
+    ) {
+      return false;
     }
-    return true;
-}
+  }
+  return true;
+};
